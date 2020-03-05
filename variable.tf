d3bb6ca5cebe316cd4022d8b9734439fc3638735
roadtoclouds <57621665+roadtoclouds@users.noreply.github.com>
2020-03-05 19:59:30 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "aws_access_key" {
-  default  = "AKIAXP42CI54UPO53BUX"
+  default  = "xxx"
 }
 
 variable "aws_secret_key" {
-  default     = "vwWEPBOtMlA8RO25auGtLbuXQcKy3vjH1au2Bwhm"
+  default     = "xxx"
 }
